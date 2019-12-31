8a85139055be14cb733ab38f7457f031de84a553
jaemyung.kim <akdldi@5>
2019-12-31 16:31:20 +0900

    cats

--- src/main/scala/com/github/windbird123/cats/chap4/CatsMonad.scala
+++ src/main/scala/com/github/windbird123/cats/chap4/CatsMonad.scala
@@ -1,5 +1,56 @@
 package com.github.windbird123.cats.chap4
 
-object CatsMonadTest {
+import scala.concurrent.{Await, Future}
 
+object CatsMonadTest {
+  import cats.Monad
+  import cats.instances.future._
+  import cats.instances.option._
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.duration._
+
+  def main(args: Array[String]): Unit = {
+    val opt1 = Monad[Option].pure(3)
+    val opt2 = Monad[Option].flatMap(opt1)(x => Some(x + 3))
+
+    val fm = Monad[Future]
+    val future = fm.flatMap(fm.pure(1))(x => fm.pure(x + 2))
+    val out = Await.result(future, 1.second)
+    println(out)
+  }
 }
+
+object MonadSyntaxTest {
+  import cats.Monad
+  import cats.instances.option._
+  import cats.syntax.flatMap._
+  import cats.syntax.functor._
+
+  def sumSquare[F[_]: Monad](a: F[Int], b: F[Int]): F[Int] =
+    for {
+      x <- a
+      y <- b
+    } yield x * x + y * y
+
+  def main(args: Array[String]): Unit = {
+    val out = sumSquare(Option(3), Option(4))
+    println(out)
+
+    // sumSquare(3, 4) 도 working 하게 하고 싶다면???
+    import cats.Id
+    sumSquare(3: Id[Int], 4 : Id[Int])
+  }
+}
+
+
+trait MyId {
+  import cats.Id
+
+  def pure[A](a: A) : Id[A] = a
+
+  def map[A, B](initial: Id[A])(f: A => B) : Id[B] = f(initial)
+
+  def flatMap[A, B](initial: Id[A])(f: A => Id[B]) : Id[B] = f(initial)
+}
+
--- src/main/scala/com/github/windbird123/cats/chap4/MonadDef_1.scala
+++ src/main/scala/com/github/windbird123/cats/chap4/MonadDef_1.scala
@@ -2,7 +2,7 @@ package com.github.windbird123.cats.chap4
 
 import scala.language.higherKinds
 
-trait Monad[F[_]] {
+trait MonadDef[F[_]] {
   def pure[A](value: A): F[A]
 
   def flatMap[A, B](value: F[A])(func: A => F[B]): F[B]
