8414ab37577a275f25167bece0f9dc81207260b6
jaemyung.kim <jaemyung.kim@navercorp.com>
2020-01-12 00:12:54 +0900

    functor

--- src/main/scala/com/github/windbird123/cats/chap3/FunctorDef_1.scala
+++ src/main/scala/com/github/windbird123/cats/chap3/FunctorDef_1.scala
@@ -8,3 +8,21 @@ trait Functor[F[_]] {
 // Functor map 은 다음 조건을 만족해야 한다.
 // 1. identity:  fa.map(a => a) == fa
 // 2. composition:  fa.map(g(f(_))) == fa.map(f).map(g)
+
+
+trait Ptr[A] {
+  self =>
+  def format(value: A): String
+
+  def contramap[B](func: B => A): Ptr[B] = new Ptr[B] {
+    override def format(value: B): String = self.format(func(value))
+  }
+}
+
+final case class Box2[A](value: A)
+
+object PtrTest {
+  implicit def boxPtr[A](implicit p: Ptr[A]) : Ptr[Box2[A]] = p.contramap(_.value)
+
+
+}
