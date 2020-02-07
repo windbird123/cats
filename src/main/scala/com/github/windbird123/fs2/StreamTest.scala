ab815b03e556da67dcd4baa0f054ebdd1e37fa24
jaemyung.kim <akdldi@5>
2020-02-07 17:00:05 +0900

    stream

--- src/main/scala/com/github/windbird123/fs2/StreamTest.scala
+++ src/main/scala/com/github/windbird123/fs2/StreamTest.scala
@@ -1,6 +1,7 @@
 package com.github.windbird123.fs2
 
-import cats.effect.IO
+import cats.effect.{Async, ContextShift, IO, Timer}
+import fs2.Chunk
 
 import scala.concurrent.ExecutionContext
 
@@ -27,7 +28,7 @@ object CreateStreamTest extends App {
     .toList
 
   // process parallel
-  implicit val cs = IO.contextShift(ExecutionContext.global)
+  implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
   fs2.Stream
     .emits[IO, Char]('A' to 'E')
     .map(
@@ -35,8 +36,35 @@ object CreateStreamTest extends App {
     )
     .parJoin(5)
 
-
   // Batching
   fs2.Stream.emits(1 to 100).chunkN(10).map(println).compile.drain
 
+  val s = fs2.Stream(1, 2) ++ fs2.Stream(3) ++ fs2.Stream(4, 5, 6)
+  val chunks = s.chunks.toList // List(Chunk(1, 2), Chunk(3), Chunk(4, 5, 6))
+
+  import scala.concurrent.duration._
+  implicit val timer: Timer[IO] = IO.timer(ExecutionContext.global)
+  fs2.Stream
+    .awakeEvery[IO](10.millis)
+    .groupWithin(100, 100.millis)
+    .evalTap(
+      chuck => IO(println(s"Processing batch of ${chuck.size} elements"))
+    )
+    .compile
+    .drain
+
+  // Asynchronous computation
+  def writeToDatabase[F[_]: Async](chunk: Chunk[Int]) : F[Unit] = Async[F].async { callback =>
+    println(s"Writing batch of $chunk to database by ${Thread.currentThread().getName}")
+    callback(Right())
+  }
+
+  fs2.Stream.emits(1 to 10000)
+      .chunkN(10)
+      .covary[IO]
+      .parEvalMap(10)(writeToDatabase[IO])
+      .compile
+      .drain
+  // parEvalMap preserves the stream ordering
+  // note: mapAsyncUnordered
 }
