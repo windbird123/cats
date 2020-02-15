460681267418c6adf3e1117f1e5f93f7173c6789
jaemyung.kim <jaemyung.kim@navercorp.com>
2020-02-15 18:50:06 +0900

    typeclass

--- src/main/scala/com/github/windbird123/cats/TypeClassTest.scala
+++ src/main/scala/com/github/windbird123/cats/TypeClassTest.scala
@@ -1,31 +1,30 @@
 package com.github.windbird123.cats
 
-import scala.util.Try
-
 trait Shape[A] {
   def area(a: A): Double
 }
 
 object Shape {
-  def area[A](a: A)(implicit inst: Shape[A]): Double = inst.area(a)
+  def apply[A](implicit inst: Shape[A]): Shape[A] = inst
+//  def area[A](a: A)(implicit inst: Shape[A]): Double = inst.area(a)
 }
 
 case class Circle(r: Double)
 case class Rect(w: Double, h: Double)
 
 object ShapeInstances {
-  implicit object circleInst extends Shape[Circle] {
+  implicit val circleInst: Shape[Circle] = new Shape[Circle] {
     override def area(circle: Circle): Double = Math.PI * circle.r * circle.r
   }
 
-  implicit object rectInst extends Shape[Rect] {
+  implicit val rectInst: Shape[Rect] = new Shape[Rect] {
     override def area(rect: Rect): Double = rect.w * rect.h
   }
 }
 
 object ShapeSyntax {
   implicit class ShapeSyntaxImpl[A](any: A) {
-    def area(implicit inst: Shape[A]): Double = Shape.area(any)
+    def area(implicit inst: Shape[A]): Double = Shape[A].area(any)
   }
 }
 
@@ -34,26 +33,10 @@ object TypeClassTest extends App {
   import ShapeSyntax._
 
   val circle = Circle(5)
-  println(Shape.area(circle))
+  println(Shape[Circle].area(circle))
   println(circle.area)
 
   val rect = Rect(3, 4)
-  println(Shape.area(rect))
+  println(Shape[Rect].area(rect))
   println(rect.area)
-}
-
-object TmpTest extends App {
-  import cats.MonadError
-  import cats.instances.try_._
-
-  def parseUser[F[_]](
-    s: String
-  )(implicit me: MonadError[F, Throwable]): F[String] = {
-    me.pure("abc")
-//    me.raiseError("error")
-  }
-
-//  implicit val me = MonadError[Try, Throwable]
-  val user = parseUser[Try]("abc")
-  println(user)
 }
