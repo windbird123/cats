6bb4bc99730b123bf0ec0b6ae21a77ab8c424919
jaemyung.kim <jaemyung.kim@navercorp.com>
2020-02-21 23:30:59 +0900

    chap9

--- src/main/scala/com/github/windbird123/cats/chap9/FoldMapImpl_1.scala
+++ src/main/scala/com/github/windbird123/cats/chap9/FoldMapImpl_1.scala
@@ -1,11 +1,31 @@
 package com.github.windbird123.cats.chap9
 
-
-
 object FoldMapImpl {
 
   import cats.Monoid
   import cats.syntax.monoid._
 
-  def foldMap[A, B: Monoid](seq : Vector[A])(f: A => B): B = seq.map(f).foldLeft(Monoid[B].empty)( _ |+| _)
+  def foldMap[A, B: Monoid](v: Vector[A])(f: A => B): B =
+    v.foldLeft(Monoid[B].empty) { (b, a) =>
+      b |+| f(a)
+    }
+
+  import cats.instances.future._
+  import cats.instances.vector._
+  import cats.syntax.foldable._
+  import cats.syntax.monoid._
+  import cats.syntax.traverse._
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.Future
+
+  def parallelFoldMap[A, B: Monoid](values: Vector[A])(f: A => B): Future[B] =
+    values
+      .grouped(3)
+      .toVector
+      .traverse { v =>
+        Future { v.foldLeft(Monoid[B].empty)(_ |+| f(_)) }
+      }
+      .map(_.combineAll)
+
 }
